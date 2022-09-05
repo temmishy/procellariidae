350a1e45f233033c086e5c625564b7b738a62924
temmishy <temmishy@temmishy.com>
2022-09-05 06:39:04 -0400

    Added Show Incidents

--- procellariidae/views.py
+++ procellariidae/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Incident
+from django.utils import timezone
+
+def incidents_list(request):
+    incidents = Incident.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'procellariidae/incidents_list.html', {'incidents': incidents})
