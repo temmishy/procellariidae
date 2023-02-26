bf6300e71fb47701eafbd8e699d3125050c6b287
temmishy <temmish17@gmail.com>
2023-02-26 18:49:11 +0700

    some fixes

--- init.sh
+++ init.sh
@@ -4,9 +4,6 @@ user=teacher
 email=teacher@example.com
 password=teacher
 
-file=db.sqlite3
-if [ -z "$file" ]; then
-  echo "from django.contrib.auth.models import User; User.objects.create_superuser('$user', '$email', '$password')" | python3 manage.py shell
-fi
-
+file=db/db.sqlite3
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('$user', '$email', '$password')" | python3 manage.py shell
 python3 manage.py migrate
