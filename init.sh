4f5f6898e4da7eafd55a12ac0b5e0c9ad667cbea
temmishy <temmish17@gmail.com>
2023-02-26 18:04:15 +0700

    Edit docker-compose and init.sh

--- init.sh
+++ init.sh
@@ -1,15 +1,12 @@
 #! /bin/sh
+# User credentials
+user=teacher
+email=teacher@example.com
+password=teacher
 
 file=db.sqlite3
-if [ -e "$file" ]; then
-  # Control will enter here if $file exists
-  rm $file
+if [ -z "$file" ]; then
+  echo "from django.contrib.auth.models import User; User.objects.create_superuser('$user', '$email', '$password')" | python3 manage.py shell
 fi
 
-# User credentials
-user=admin
-email=admin@example.com
-password=pass
-
 python3 manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('$user', '$email', '$password')" | python3 manage.py shell
