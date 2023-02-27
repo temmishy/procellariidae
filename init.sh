081579bcf8c5467589471b0a5e5b77b6af799712
temmishy <temmish17@gmail.com>
2023-02-27 12:39:50 +0700

    some fixes init.sh

--- init.sh
+++ init.sh
@@ -4,9 +4,16 @@ python3 manage.py migrate
 # User credentials
 user=teacher
 email=admin@example.com
-password=teacher
+password=12345
+
+array_users = ("analyst1", "analyst2", "response1", "response2")
 
 file=db/db.sqlite3
 
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('$user', '$email', '$password')" | python3 manage.py shell
+
+for usr in ${array_users[@]}; do
+  echo "from django.contrib.auth.models import User; User.objects.create_user('$usr', '$email', '$password')" | python3 manage.py shell
+done
+
 python3 manage.py migrate
