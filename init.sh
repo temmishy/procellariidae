b4c279127364161467d463857311062e652296ad
temmishy <temmish17@gmail.com>
2023-02-27 11:46:53 +0700

    some fixes docker-compose

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 # User credentials
-user=teacher
-email=teacher@example.com
-password=teacher
+user=admin
+email=admin@example.com
+password=pass
 
 file=db/db.sqlite3
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('$user', '$email', '$password')" | python3 manage.py shell
